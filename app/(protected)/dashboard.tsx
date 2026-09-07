a94c62f98acefa5a1e3194293b9ec0bb20b9ea25
Karlos <loboscarlos1704@gmail.com>
2026-09-07 17:15:19 -0600

    avances

--- app/(protected)/dashboard.tsx
+++ app/(protected)/dashboard.tsx
@@ -1,8 +1,62 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import Drawer from "expo-router/drawer";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
+import { useDrawer } from "../src/admin/viewmodels/use-drawer";
+export default function DashboardScreen() {
 
-export default function DashboardScreen(){
-    <View>
-        <Text>hELLO FROM DASHBOARD</Text>
-    </View>
+    const navigation = useNavigation()
+    const { profile } = useDrawer()
+    const openMenu = () => {
+        navigation.dispatch(DrawerActions.openDrawer())
+    }
+
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Drawer.Screen options={{ headerShown: false }} />
+            <View style={styles.header}>
+                <TouchableOpacity onPress={openMenu} style={{ padding: 15 }}>
+                    <Ionicons name="menu-outline" size={30} color="#005C3A" />
+                </TouchableOpacity>
+                <Text style={styles.logoText}>ShopEase</Text>
+                <TouchableOpacity style={styles.profileContainer}>
+                    <Image 
+                    source={{uri:profile.avatarUrl}}
+                    style={styles.avatarImage}
+                    />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#F9FAFB"
+    },
+    header: {
+        flexDirection: "row",
+        alignContent: "center",
+        justifyContent: "space-between",
+        paddingHorizontal:15,
+        paddingVertical:14
+    },
+    logoText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#005C3A"
+    },
+    profileContainer:{
+        borderRadius:20,
+        overflow:"hidden",
+        borderWidth:1
+    },
+    avatarImage:{
+        width:40,
+        height:40
+    }
+})
